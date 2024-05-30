9ed768c7a75dfe77441f04315e4ebc11005b50cd
DevPatel711 <devdpatel711@gmail.com>
2024-05-30 06:42:46 +0530

    url change

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:4001",
+			const socket = io("https://chat-application-1-jy7g.onrender.com",
              {
 				query: {
 					userId: authUser._id,
